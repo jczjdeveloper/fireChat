b87f6cf0a89305eb58f700f8ee93369b19a05ebf
jczjdeveloper <jczjdeveloper@gmail.com>
2018-04-26 18:40:47 +0800

    outstanding: to fix material button component

--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -2,6 +2,15 @@ import React, {PropTypes} from 'react';
 
 // Import css
 import './Login';
+import {
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia,
+  CardTitle,
+  CardText
+} from 'material-ui/Card';
+import Button from 'material-ui/Button';
 
 export default class Login extends React.Component {
   constructor(props) {
@@ -10,7 +19,18 @@ export default class Login extends React.Component {
 
   render() {
     return (
-      <div>This is the login component!</div>
+      <div>
+        <Card>
+          <CardTitle title="Card title" subtitle="Card subtitle"/>
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          </CardText>
+          <CardActions>
+            <Button label="Action1"/>
+            <Button label="Action2"/>
+          </CardActions>
+        </Card>
+      </div>
     );
   }
 }
